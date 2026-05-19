bfc2afe989acb42328e4d4831c7614d1760639f4
ROSHAN MARANDI <rpoul2614@gmail.com>
2026-05-19 13:27:10 +0530

    Add local config example

---
+++ includes/config.local.example.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'dbexam_modern',
+    'db_user' => 'root',
+    'db_pass' => '',
+    'app_name' => 'Exam Paper Generator',
+];
